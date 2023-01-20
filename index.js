d6db9293a763c3e52e0f2d1d84deb8e8eddf123a
bencha27 <115957643+bencha27@users.noreply.github.com>
2023-01-20 14:35:30 -0700

    Created separate functions to generate each README section and moved them to utils/generateMarkdown.js; added/updated comments in JS files

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-// TODO: Include packages needed for this application
+// Packages for the application
 const inquirer = require("inquirer");
 const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
-// TODO: Create an array of questions for user input
+// Array of questions for user input
 const questions = [
   "Enter the title of your project.",
   "Enter a short description of the project.",
@@ -15,91 +16,13 @@ const questions = [
   "Enter your email address.",
 ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, {title, description, installation, usage, license, contribute, tests, email, github}) {
-  const licenseBadge = generateLicenseBadge(license);
-
-  let readmeContent = ``;
-
-  if (title === "") {
-    readmeContent += `# Project Title
-
-`;
-  } else {
-    readmeContent += `# ${title}
-
-`;
-  }
-
-  if (description !== "") {
-    readmeContent += `## Description
-${description}
-
-`;
-  }
-
-  readmeContent += `## Table of Contents
-- [Installation](#installation)
-- [Usage](#usage)
-- [License](#license)
-- [How to Contribute](#how-to-contribute)
-- [Tests](#tests)
-- [Questions](#questions)
-
-`;
-
-  if (installation !== "") {
-    readmeContent += `## Installation
-${installation}
-
-`
-  }
-
-  if (usage !== "") {
-    readmeContent += `## Usage
-${usage}
-
-`;
-  }
-
-  if (licenseBadge !== "") {
-    readmeContent += `## License
-${licenseBadge}
-
-`;
-  }
-
-  if (contribute !== "") {
-    readmeContent += `## How to Contribute
-${contribute}
-
-`;
-  }
-
-  if (tests !== "") {
-    readmeContent += `## Tests
-${tests}
-
-`;
-  }
-
-  if (email !== "" && github !== "") {
-    readmeContent += `## Questions
-If you have any questions, please contact me at [${email}](mailto:${email}).\\
-[GitHub](https://github.com/${github})`;
-  } else if (email !== "" && github === "") {
-    readmeContent += `## Questions
-If you have any questions, please contact me at [${email}](mailto:${email}).`;
-  } else if (email === "" && github !== "") {
-    readmeContent += `## Questions
-[GitHub](https://github.com/${github})`;
-  }
-
-  fs.writeFile(fileName, readmeContent, (err) => 
+// Function to write README file
+function writeToFile(data) {
+  fs.writeFile("./dist/README.md", generateMarkdown(data), (err) => 
      err ? console.error(err) : console.log("Successfully created README.md!"));
 };
 
-// TODO: Create a function to initialize app
+// Function to initialize app
 function init() {
   inquirer
     .prompt([
@@ -150,22 +73,7 @@ function init() {
         message: questions[8],
       },
     ])
-    .then((response) => {writeToFile("README.md", response)});
-}
-
-function generateLicenseBadge(license) {
-  switch (license) {
-    case "MIT License":
-      return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
-    case "Apache License 2.0":
-      return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
-    case "ISC License":
-      return "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)";
-    case "GNU General Public License v3.0":
-      return "[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)";
-    default:
-      return "";
-  }
+    .then((response) => {writeToFile(response)});
 }
 
 // Function call to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,185 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+// Function that returns the title section
+function renderTitleSection(title) {
+  if (title !== "") {
+    return `# ${title}
 
 `;
+  } else {
+    return `# Project Title
+
+`;
+  }
+}
+
+// Function that returns the description section
+function renderDescriptionSection(description) {
+  if (description !== "") {
+    return `## Description
+${description}
+`;
+  } else {
+    ``;
+  }
+}
+
+// Function that returns the table of contents section
+function renderTableOfContents(installation, usage, license, contribute, tests, email, github) {
+  let tableOfContents = `## Table of Contents`;
+  if (installation !== "") {
+    tableOfContents += `
+- [Installation](#installation)`;
+  }
+
+  if (usage !== "") {
+    tableOfContents += `
+- [Usage](#usage)`;
+  }
+
+  if (license !== "") {
+    tableOfContents += `
+- [License](#license)`;
+  }
+
+  if (contribute !== "") {
+    tableOfContents += `
+- [How to Contribute](#how-to-contribute)`;
+  }
+
+  if (tests !== "") {
+    tableOfContents += `
+- [Tests](#tests)`;
+  }
+
+  if (email !== "" || github !== "") {
+    tableOfContents += `
+- [Questions](#questions)`;
+  }
+  tableOfContents += `
+`
+  return tableOfContents;
+}
+
+// Function that returns the installation section
+function renderInstallationSection(installation) {
+  if (installation !== "") {
+    return `## Installation
+${installation}
+`;
+  } else {return ``}
+}
+
+// Function that returns the usage section
+function renderUsageSection(usage) {
+  if (usage !== "") {
+    return `## Usage
+${usage}
+`;
+  } else {return ""}
+}
+
+// Function that returns a license badge based on which license is passed in
+function renderLicenseBadge(license) {
+  if (license === "None") {
+    return "";
+  } else {
+    renderLicenseLink(license);
+  }
+
+  switch (license) {
+    case "MIT License":
+      return "![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)";
+    case "Apache License 2.0":
+      return "![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)";
+    case "ISC License":
+      return "![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)";
+    case "GNU General Public License v3.0":
+      return "![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)";
+    default:
+      return "";
+  }
+}
+
+// Function that returns the license link
+function renderLicenseLink(license) {
+  switch (license) {
+    case "MIT License":
+      return "https://opensource.org/licenses/MIT";
+    case "Apache License 2.0":
+      return "https://opensource.org/licenses/Apache-2.0";
+    case "ISC License":
+      return "https://opensource.org/licenses/ISC";
+    case "GNU General Public License v3.0":
+      return "https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html";
+    default:
+      return "";
+  }
+}
+
+// Function that returns the license section of README
+function renderLicenseSection(license) {
+  const licenseBadge = renderLicenseBadge(license);
+  const licenseLink = renderLicenseLink(license);
+
+  if (license !== "None") {
+    return `## License
+[${licenseBadge}](${licenseLink})
+`;
+  } else {return ``}
+}
+
+// Function that returns the contribute section
+function renderContributeSection(contribute) {
+  if (contribute !== "") {
+    return `## How to Contribute
+${contribute}
+`;
+  } else {return ``}
+}
+
+// Function that returns the tests section
+function renderTestsSection(tests) {
+  if (tests !== "") {
+    return `## Tests
+${tests}
+`;
+  } else {return ``}
+}
+
+// Function that returns the email address
+function renderEmail(email) {
+  if (email !== "") {
+    return `If you have any questions, please contact me at [${email}](mailto:${email}).\\`
+  } else {return ``}
+}
+
+// Function that returns the GitHub link
+function renderGithub(github) {
+  if (github !== "") {
+    return `[GitHub](https://github.com/${github})`
+  } else {return ``}
+}
+
+// Function that returns the questions section
+function renderQuestionsSection(email, github) {
+  if (email !== "" || github !== "") {
+    return `## Questions
+${renderEmail(email)}
+${renderGithub(github)}`;
+  } else {return ``}
+}
+
+// Function to generate markdown for README
+function generateMarkdown({title, description, installation, usage, license, contribute, tests, email, github}) {
+  let readmeText = `${renderTitleSection(title)}
+${renderDescriptionSection(description)}
+${renderTableOfContents(installation, usage, license, contribute, tests, email, github)}
+${renderInstallationSection(installation)}
+${renderUsageSection(usage)}
+${renderLicenseSection(license)}
+${renderContributeSection(contribute)}
+${renderTestsSection(tests)}
+${renderQuestionsSection(email, github)}`;
+  return readmeText;
 }
 
 module.exports = generateMarkdown;
